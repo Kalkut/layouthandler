c6c5791dbcbfdf7320ea6b139dda52b22c0ab989
Bérenger <berenger.makita@gmail.com>
2014-08-05 15:28:55 +0200

    Version temporaire de sauvegarde avant checkout

--- libs/Case.js
+++ libs/Case.js
@@ -12,7 +12,7 @@ var onimagesload = function (imgs,callback) {
 	if (c === l) callback();
 }
 
-sand.define('Case', function (r) {
+sand.define('Case',["Geo/*"], function (r) {
 
 	return Seed.extend({
 		'+options' : {
@@ -30,7 +30,9 @@ sand.define('Case', function (r) {
 			this.fit = options.fit
 			this.pos = options.pos;
 			this.keyPressed = {};
-			this.cursorOver = false
+			this.cursorOver = false;
+			this.imgRect;
+			this.divRect;
 			
 			this.div = toDOM({
 				tag : 'div.' + options.prefix + "-case-idle",
@@ -114,17 +116,9 @@ sand.define('Case', function (r) {
 					}
 				}.bind(this));*/
 
-
-				/*this.div.onkeydown = function (e) {
-					this.shiftPressed = e.keyCode === 18;
-					console.log(this.shiftPressed);
-				}.bind(this)*/
-
 				document.body.addEventListener('keydown', function (e) {
 					
-					this.keyPressed[e.keyCode] = [true,this.selected];
-					console.log(this.keyPressed[e.keyCode])
-					
+					this.keyPressed[e.keyCode] = [true,this.selected];					
 				}.bind(this))
 
 				document.body.addEventListener('keyup', function (e) {
@@ -132,11 +126,10 @@ sand.define('Case', function (r) {
 				}.bind(this))
 
 				this.div.addEventListener('mousewheel', function (e) {
-					//console.log(this.keyPressed[16]);
-					//console.log(this.cursorOver)
-					//console.log(this.keyPressed[16] && this.cursorOver)
+					//console.log('what ?')
 					if(this.keyPressed[16]){
 						e.preventDefault();
+
 						var length = parseInt(this.img.style.width);
 						var delta = Math.max(-1, Math.min(1, (e.wheelDelta || -e.detail)));
 						length+= delta*5;
@@ -146,18 +139,23 @@ sand.define('Case', function (r) {
 
 
 
-				this.img.onclick = function (e) {
-					//console.log(e.clientX - this.offsetLeft, e.clientY - this.offsetTop);
+				this.img.onmousemove = function (e) {
+					//console.log(e.clientX, e.clientY);
+					//console.log(this.offsetParent.offsetLeft)
 				}
 				
 				var imgMove = function (e) {
+					//console.log(e.clientX - this.div.offsetLeft + Math.abs(parseInt(this.img.style.left)), e.clientY - this.div.offsetTop + Math.abs(parseInt(this.img.style.top)));
 					var deltaX = e.clientX - this.img.width/2 - this.posClick[0];
 					var deltaY = e.clientY- this.img.height/2 - this.posClick[1];
+					var delta = [deltaX,deltaY];
+					var potentialRect = this.imgRect.move({vector : delta});
 					if(this.clicking) {
-						if (!this.fit) {
-							this.img.style.left = Math.max((Math.min(parseInt(this.img.style.left) + deltaX,0)),-Math.abs(parseInt(this.img.style.width)-parseInt(this.div.style.width)));
-							this.img.style.top = Math.max(Math.min(parseInt(this.img.style.top)+ deltaY,0),-Math.abs(parseInt(this.img.style.height)-parseInt(this.div.style.height)));
-						}else {
+						if (!this.fit && (potentialRect.segX.c2 >= parseInt(this.div.style.width) && potentialRect.segX.c1 <= 0 && potentialRect.segY.c1 <= 0 && potentialRect.segY.c2 >= parseInt(this.div.style.height))) {
+							this.img.style.left = parseInt(this.img.style.left) + deltaX;
+							this.img.style.top = parseInt(this.img.style.top) + deltaY;
+							this.imgRect = potentialRect;
+						}else if(this.fit) {
 							this.img.style.left = Math.max(Math.min(parseInt(this.div.style.width),parseInt(this.img.style.left) + deltaX),-Math.min(parseInt(this.div.style.width)));
 							this.img.style.top = Math.max(Math.min(parseInt(this.div.style.height),parseInt(this.img.style.top) + deltaY),-Math.min(parseInt(this.div.style.height)));
 						}
@@ -170,6 +168,7 @@ sand.define('Case', function (r) {
 				}.bind(this)
 
 				onimagesload([this.img], function() {
+
 					if(this.pos){
 						this.ratio = parseInt(this.img.naturalHeight)/parseInt(this.img.naturalWidth);
 						this.img.style.width = this.pos[2];
@@ -183,33 +182,48 @@ sand.define('Case', function (r) {
 
 						var height = parseInt(this.div.style.height);
 						var width = parseInt(this.div.style.width);
+
 						var ratioDiv = height/width;
 
 						if (this.ratio > ratioDiv) this.zoom(width,false,true);
 						else this.zoom(height,true,true);
+
+
 					}
 
+					var imgX = parseInt(this.img.style.left);
+					var imgY = parseInt(this.img.style.top);
+
+					var segX = new r.Geo.Seg(imgX, imgX + parseInt(this.img.style.width));
+					var segY = new r.Geo.Seg(imgY, imgY + parseInt(this.img.style.height));
+
+					this.imgRect = new r.Geo.Rect({ segX : segX, segY : segY});
+
 				}.bind(this))
-				
-				this.div.onmousemove = imgMove.bind(this);
-			}	
-		},
 
-		zoom : function (newLength,widthOrHeight,init) {
-			if(!widthOrHeight) {
-				this.img.style.width = newLength;
-				this.img.style.height = newLength*this.ratio;
-			}
-			else {
-				this.img.style.height = newLength;
-				this.img.style.width = newLength/this.ratio;
-			}
-			if(!init){
-				this.fire('imgMoved',this.img.style.left,this.img.style.top,this.img.style.width,this.img.style.height);
-				this.fire('update:position',parseInt(this.img.style.left),parseInt(this.img.style.top),parseInt(this.img.style.width),parseInt(this.img.style.height));
-			}
-		}
+this.div.onmousemove = imgMove.bind(this);
+}	
+},
+
+zoom : function (newLength,widthOrHeight,init) {
+	if(!widthOrHeight) {
+		this.img.style.width = newLength;
+		this.img.style.height = newLength*this.ratio;
+	}
+	else {
+		this.img.style.height = newLength;
+		this.img.style.width = newLength/this.ratio;
+	}
+	if(!init){
+		this.fire('imgMoved',this.img.style.left,this.img.style.top,this.img.style.width,this.img.style.height);
+		this.fire('update:position',parseInt(this.img.style.left),parseInt(this.img.style.top),parseInt(this.img.style.width),parseInt(this.img.style.height));
+	}
+}
+
+/*zoom : function (delta) {
+	
+}*/
 
 
-	})
+})
 })
